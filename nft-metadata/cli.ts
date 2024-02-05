d2b5b7be747b237f87a561a02042ae5dbb85057f
Joey Kheireddine <joey.kheireddine@outlook.com>
2024-02-05 23:09:31 +0100

    upd: more work towards metadata package

--- nft-metadata/cli.ts
+++ nft-metadata/cli.ts
@@ -25,21 +25,18 @@ const options = program.opts();
 
 if (!options?.network || !options?.contractaddress || !options?.type) {
   console.error(`Missing one of the required arguments.`);
-  // @ts-expect-error
   process.exit(1);
 }
 
 const network = program?.args?.[0]?.toLowerCase();
 if (network !== "porcini" && network !== "root") {
   console.error(`Network can only be root or porcini`);
-  // @ts-expect-error
   process.exit(1);
 }
 
 let contractAddress = program?.args?.[1];
 if (!isAddress(contractAddress)) {
   console.error(`Provided contractAddress is an invalid address`);
-  // @ts-expect-error
   process.exit(1);
 } else {
   contractAddress = getAddress(program?.args?.[1]);
@@ -48,7 +45,6 @@ if (!isAddress(contractAddress)) {
 const tokenType = program?.args?.[2]?.toLowerCase();
 if (tokenType !== "erc721" && tokenType !== "erc1155") {
   console.error(`Provided type can obly be erc721 or erc1155`);
-  // @ts-expect-error
   process.exit(1);
 }
 
