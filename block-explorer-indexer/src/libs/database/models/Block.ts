c0c87319ee2011094a3890f1fde8eca545a0cfb8
Joey Kheireddine <joey.kheireddine@outlook.com>
2024-02-05 01:00:57 +0100

    upd: added missing index

--- block-explorer-indexer/src/libs/database/models/Block.ts
+++ block-explorer-indexer/src/libs/database/models/Block.ts
@@ -24,7 +24,7 @@ const schema = new Schema<IBlock, Mongoose.Model<IBlock>>({
 
 schema.index({ number: 1 }, { unique: true });
 schema.index({ timestamp: 1 });
-
+schema.index({ number: -1 });
 schema.index({ isFinalized: 1 });
 schema.index({ isFinalized: 1, number: 1 });
 schema.index({ 'evmBlock.miner': 1 });
