28224d1e1d9bd5b15ea887986151defcbbd09ad6
Joey Kheireddine <joey.kheireddine@outlook.com>
2024-02-06 00:32:16 +0100

    upd: reworekd getTokenMetadata

--- block-explorer-indexer/src/libs/token-data/index.ts
+++ block-explorer-indexer/src/libs/token-data/index.ts
@@ -1,18 +1,15 @@
-import { Address, getAddress } from "viem";
-import fs from "node:fs/promises";
+import fs from 'node:fs/promises';
+import { Address, getAddress } from 'viem';
 
-export const getTokenMetadata = async (
-  contractAddress: Address,
-  tokenId: number,
-  network: "root" | "porcini"
-) => {
-  const fileDir = `./src/libs/token-data/blockchains/${network}/${getAddress(
-    contractAddress
-  )}.json`;
-  const readData = await fs.readFile(fileDir, "utf-8");
+export const getTokenMetadata = async (contractAddress: Address, tokenId: number, network: 'root' | 'porcini') => {
+  const fileDir = `./src/libs/token-data/blockchains/${network}/${getAddress(contractAddress)}.json`;
+  const readData = await fs.readFile(fileDir, 'utf-8').catch(() => {
+    return null;
+  });
+  if (!readData) return {};
   const data = structuredClone(JSON.parse(readData));
 
   const metadata = data?.find((a) => Number(a?.tokenId) === Number(tokenId));
 
-  return metadata || null;
+  return metadata || {};
 };
