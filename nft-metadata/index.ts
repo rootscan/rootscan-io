bc8323ffccfc41576af797633c50a1f1c5daf5ee
Joey Kheireddine <joey.kheireddine@outlook.com>
2024-02-05 23:12:46 +0100

    upd: export function that allows us to search for metadata

---
+++ nft-metadata/index.ts
@@ -0,0 +1,18 @@
+import { Address, getAddress } from "viem";
+import fs from "node:fs/promises";
+
+export const getTokenMetadata = async (
+  contractAddress: Address,
+  tokenId: number,
+  network: "root" | "porcini"
+) => {
+  const fileDir = `./blockchains/${network}/${getAddress(
+    contractAddress
+  )}.json`;
+  const readData = await fs.readFile(fileDir, "utf-8");
+  const data = structuredClone(JSON.parse(readData));
+
+  const metadata = data?.find((a) => Number(a?.tokenId) === Number(tokenId));
+
+  return metadata || null;
+};
