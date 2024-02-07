c1a4b652c683a2b1cde1f73c5196f7c021b59a07
Joey Kheireddine <joey.kheireddine@outlook.com>
2024-02-07 18:48:56 +0100

    upd: update fonts

--- block-explorer/lib/fonts.ts
+++ block-explorer/lib/fonts.ts
@@ -3,19 +3,19 @@ import localFont from "next/font/local"
 export const fontLocal = localFont({
   src: [
     {
-      path: "./fonts/ObjektivMk1-Medium.woff2",
+      path: "../public/fonts/ObjektivMk1-Medium.woff2",
       weight: "400",
     },
     {
-      path: "./fonts/ObjektivMk1-Medium.woff2",
+      path: "../public/fonts/ObjektivMk1-Medium.woff2",
       weight: "500",
     },
     {
-      path: "./fonts/ObjektivMk1_Trial_SBd.ttf",
+      path: "../public/fonts/ObjektivMk1_Trial_SBd.ttf",
       weight: "600",
     },
     {
-      path: "./fonts/ObjektivMk1-Bold.woff2",
+      path: "../public/fonts/ObjektivMk1-Bold.woff2",
       weight: "700",
     },
   ],
