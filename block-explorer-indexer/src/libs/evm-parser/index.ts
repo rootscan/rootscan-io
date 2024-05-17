34301f75a0f7d332747955eae424344e07a2740e
Yaroslav Savaryn <yariksav@gmail.com>
2024-05-17 12:43:22 +0300

    restore code

--- block-explorer-indexer/src/libs/evm-parser/index.ts
+++ block-explorer-indexer/src/libs/evm-parser/index.ts
@@ -114,7 +114,7 @@ export const parseEventsFromEvmTx = async (tx: TransactionReceipt & Transaction)
         // ERC721 Transfer
         if (tokenDetails?.type === 'ERC721' && event?.eventName === 'Transfer') {
           const tokenId = event?.value || event?.tokenId;
-          event.tokenId = Number(tokenId) > 2_000_000_000 ? tokenId : Number(tokenId);
+          event.tokenId = String(tokenId).length > 9 ? tokenId?.toString() : Number(tokenId);
           event.name = tokenDetails.name;
           event.symbol = tokenDetails.symbol;
           event.type = 'ERC721';
