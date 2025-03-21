7a08522f13e9279b5551346103a10097792c86a6
sivzh <sivzh@gmail.com>
2025-03-21 09:18:18 +0100

    init sentry web

--- block-explorer/next.config.ts
+++ block-explorer/next.config.ts
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
+import { withSentryConfig } from '@sentry/nextjs';
+import { NextConfig } from 'next';
 
-module.exports = {
+const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
     dangerouslyAllowSVG: true,
@@ -82,6 +83,8 @@ module.exports = {
   },
   env: {
     CHAIN_ID: process.env.CHAIN_ID,
+    ENVIRONMENT: process.env.ENVIRONMENT,
+    SENTRY_DSN: process.env.SENTRY_DSN,
   },
   reactStrictMode: true,
   async redirects() {
@@ -109,3 +112,36 @@ module.exports = {
     ];
   },
 };
+
+export default withSentryConfig(nextConfig, {
+  // For all available options, see:
+  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+
+  org: 'rootscan-io',
+  project: 'block-explorer-web',
+  sentryUrl: 'http://sentry.sanctum.ai/',
+
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
+
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
+
+  // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+  // This can increase your server load as well as your hosting bill.
+  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+  // side errors will fail.
+  // tunnelRoute: "/monitoring",
+
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+
+  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+  // See the following for more information:
+  // https://docs.sentry.io/product/crons/
+  // https://vercel.com/docs/cron-jobs
+  automaticVercelMonitors: true,
+});
