aeaff41cbecc3cea26539593289b125e8e70aebb
edmoiseenkov <edmoiseenkov@gmail.com>
2025-05-30 22:12:50 -0700

    feat: update block page

--- block-explorer/app/events/[eventId]/layout.tsx
+++ block-explorer/app/events/[eventId]/layout.tsx
@@ -19,17 +19,17 @@ export default async function Layout({ children, params }: LayoutProps) {
   const { eventId } = await params;
 
   return (
-    <Container>
-      <div className="flex flex-col gap-4">
+    <Container className="flex flex-col gap-6">
+      <div className="space-y-4">
         <Breadcrumbs />
         <SectionTitle>Event {eventId}</SectionTitle>
         <TestnetWarning>
           <p className="text-sm text-red-500">
-            [ This is a <strong>Testnet</strong> event only ]
+            [This is a <strong>Testnet</strong> event only]
           </p>
         </TestnetWarning>
-        {children}
       </div>
+      {children}
     </Container>
   );
 }
--- block-explorer/components/json-viewer.tsx
+++ block-explorer/components/json-viewer.tsx
@@ -5,11 +5,17 @@ import ReactJson from 'react18-json-view';
 import 'react18-json-view/src/dark.css';
 import 'react18-json-view/src/style.css';
 
-export default function JsonViewer({ json }) {
+import { cn } from '@/lib/utils.ts';
+
+interface JsonViewer extends Omit<React.HTMLAttributes<HTMLDivElement>, 'children'> {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  json: any;
+}
+export default function JsonViewer({ json, className, ...props }: JsonViewer) {
   return (
     <Suspense fallback={'Loading...'}>
-      <div className="overflow-x-auto rounded-lg bg-black/5 p-3 dark:bg-white/5">
-        <ReactJson src={json} theme="a11y" collapsed={true} />
+      <div className={cn('overflow-x-auto rounded-lg bg-surface-bg p-2', className)} {...props}>
+        <ReactJson src={json} theme="a11y" CopyComponent={() => null} />
       </div>
     </Suspense>
   );
