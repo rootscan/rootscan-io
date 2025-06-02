57befcc8c52b7ba91dcff151bd8f1ca9bcc173ec
edmoiseenkov <edmoiseenkov@gmail.com>
2025-06-02 14:51:37 -0700

    feat: update address/token-balances page

--- block-explorer/app/addresses/[address]/balances/page.tsx
+++ block-explorer/app/addresses/[address]/balances/page.tsx
@@ -4,9 +4,9 @@ import AddressDisplay from '@/components/address-display';
 import { ErrorAlert } from '@/components/error-alert';
 import OnlyMainnet from '@/components/layouts/only-mainnet';
 import NoData from '@/components/no-data';
-import PaginationSuspense from '@/components/pagination-suspense';
 import TokenDisplay from '@/components/token-display';
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import { TableNavigation } from '@/components/ui/table-navigation.tsx';
 import { ApiCommand, request } from '@/lib/api';
 import { formatNumber, formatNumberDollars, getPaginationData, handleRequestResult } from '@/lib/utils';
 import { PageProps } from '@/types/page';
@@ -33,64 +33,66 @@ export default async function Page({ params, searchParams }: PageProps) {
     if (!balances?.length) return <NoData />;
 
     return (
-      <div className="flex flex-col gap-6">
-        <PaginationSuspense pagination={getPaginationData(data)} />
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Asset</TableHead>
-              <TableHead>Symbol</TableHead>
-              <TableHead>Contract Address</TableHead>
-              <TableHead>Balance</TableHead>
-              <TableHead>Price</TableHead>
-              <TableHead>Value</TableHead>
+      <Table>
+        <TableCaption>
+          <TableNavigation pagination={getPaginationData(data)}>
+            {!data.skipFullCount && <p>Showing {data.totalDocs} tokens</p>}
+          </TableNavigation>
+        </TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Asset</TableHead>
+            <TableHead>Symbol</TableHead>
+            <TableHead>Contract Address</TableHead>
+            <TableHead>Balance</TableHead>
+            <TableHead>Price</TableHead>
+            <TableHead>Value</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {balances.map((token) => (
+            <TableRow key={token.contractAddress}>
+              <TableCell>
+                <TokenDisplay token={token.tokenDetails} hideCopyButton overrideImageSizeClass="size-8 mr-2" />
+              </TableCell>
+              <TableCell>{token.tokenDetails?.symbol ? token.tokenDetails?.symbol : '-'}</TableCell>
+              <TableCell>
+                <AddressDisplay address={token.contractAddress} useShortenedAddress isContract />
+              </TableCell>
+              <TableCell>
+                {!isNaN(token.balance) ? (
+                  <div>
+                    {token.balance
+                      ? formatNumber(Number(formatUnits(BigInt(token.balance), token.tokenDetails?.decimals || 0)))
+                      : '0'}
+                  </div>
+                ) : null}
+              </TableCell>
+              <TableCell>
+                <OnlyMainnet fallback={'-'}>
+                  {token.tokenDetails?.priceData?.price
+                    ? formatNumberDollars(token.tokenDetails?.priceData?.price)
+                    : '-'}
+                </OnlyMainnet>
+              </TableCell>
+              <TableCell>
+                <OnlyMainnet fallback={'-'}>
+                  {token.balance && token.tokenDetails?.priceData?.price ? (
+                    <Fragment>
+                      {formatNumberDollars(
+                        Number(formatUnits(BigInt(token.balance), token.tokenDetails?.decimals || 0)) *
+                          Number(token.tokenDetails?.priceData?.price),
+                      )}
+                    </Fragment>
+                  ) : (
+                    '-'
+                  )}
+                </OnlyMainnet>
+              </TableCell>
             </TableRow>
-          </TableHeader>
-          <TableBody>
-            {balances.map((token) => (
-              <TableRow key={token.contractAddress}>
-                <TableCell>
-                  <TokenDisplay token={token.tokenDetails} hideCopyButton overrideImageSizeClass="size-10 mr-2" />
-                </TableCell>
-                <TableCell>{token.tokenDetails?.symbol ? token.tokenDetails?.symbol : '-'}</TableCell>
-                <TableCell>
-                  <AddressDisplay address={token.contractAddress} useShortenedAddress isContract />
-                </TableCell>
-                <TableCell>
-                  {!isNaN(token.balance) ? (
-                    <div>
-                      {token.balance
-                        ? formatNumber(Number(formatUnits(BigInt(token.balance), token.tokenDetails?.decimals || 0)))
-                        : '0'}
-                    </div>
-                  ) : null}
-                </TableCell>
-                <TableCell>
-                  <OnlyMainnet fallback={'-'}>
-                    {token.tokenDetails?.priceData?.price
-                      ? formatNumberDollars(token.tokenDetails?.priceData?.price)
-                      : '-'}
-                  </OnlyMainnet>
-                </TableCell>
-                <TableCell>
-                  <OnlyMainnet fallback={'-'}>
-                    {token.balance && token.tokenDetails?.priceData?.price ? (
-                      <Fragment>
-                        {formatNumberDollars(
-                          Number(formatUnits(BigInt(token.balance), token.tokenDetails?.decimals || 0)) *
-                            Number(token.tokenDetails?.priceData?.price),
-                        )}
-                      </Fragment>
-                    ) : (
-                      '-'
-                    )}
-                  </OnlyMainnet>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </div>
+          ))}
+        </TableBody>
+      </Table>
     );
   } catch (error) {
     return <ErrorAlert error={error instanceof Error ? error : new Error('An unexpected error occurred')} />;
