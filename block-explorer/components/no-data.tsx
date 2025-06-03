7e32ba42de2c620937723df0a6388441501b3f21
edmoiseenkov <edmoiseenkov@gmail.com>
2025-06-03 13:30:44 -0700

    feat: update tooltip component

--- block-explorer/components/no-data.tsx
+++ block-explorer/components/no-data.tsx
@@ -3,7 +3,7 @@ import { Card, CardContent } from '@/components/ui/card';
 export default function NoData() {
   return (
     <Card>
-      <CardContent className="grid h-[33vh] select-none place-items-center py-10 text-center text-muted-foreground">
+      <CardContent className="grid h-[340px] select-none place-items-center py-10 text-center text-sm text-muted-foreground">
         There is no data available.
       </CardContent>
     </Card>
